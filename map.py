69f6961cb42fceef39307ba1692fbebfcb5b0fbd
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2021-01-28 13:12:57 +0100

    map() function sample

---
+++ map.py
@@ -0,0 +1,6 @@
+# map() function sample
+
+a = [1, 2, 3, 4, 5]
+
+b = list(map(lambda x: x*2, a))  # [2, 4, 6, 8, 10]
+print(b)
---
+++ python3/websockets/readme.md
@@ -0,0 +1,7 @@
+install websocket library with
+
+```bash
+sudo pip3 install websockets
+```
+
+command
