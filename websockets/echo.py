a936f30690e0f10fcd932eb9158ae4c28efaf52e
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2022-05-02 15:28:08 +0200

    websocket echo server sample

---
+++ websockets/echo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""echo server from https://websockets.readthedocs.io/en/stable/
+
+to test  server run build-in client with
+
+```console
+$python -m websockets ws://localhost:8765/
+Connected to ws://localhost:8765/.
+> Hello world!
+< Hello world!
+Connection closed: 1000 (OK).
+```
+
+"""
+
+import asyncio
+import websockets
+
+async def echo(websocket, path):
+	async for message in websocket:
+		await websocket.send(message)
+
+async def main():
+	async with websockets.serve(echo, "localhost", 8765):
+		await asyncio.Future()  # run forever
+
+asyncio.run(main())
